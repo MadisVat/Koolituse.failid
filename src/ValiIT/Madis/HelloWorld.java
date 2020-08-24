a0ce2d8d6965aaff95812650fb6ae1266a528bdf
Madis Vatko <mad1s@hotmail.com>
2020-08-24 13:05:15 +0300

    ver 1

--- src/ValiIT/Madis/HelloWorld.java
+++ src/ValiIT/Madis/HelloWorld.java
@@ -3,5 +3,7 @@ package ValiIT.Madis;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(String.format("Hello World, %s!", args[0]));
+        System.out.println("Hello 2");
+
     }
 }
