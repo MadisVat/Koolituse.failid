25056c446adccddcff14402a192cba35ddeaa3e8
Madis Vatko <mad1s@hotmail.com>
2020-08-26 15:50:03 +0300

    ver *20-08-26

--- src/ValiIT/Madis/Lesson3.java
+++ src/ValiIT/Madis/Lesson3.java
@@ -104,6 +104,27 @@ public class Lesson3 {
         // a = "Test";
         // return tseT";
         return "";
+
+        /*
+        * String numbers = "01010101"
+char[] charNumbers = numbers.toCharArray();
+char[] resultNumbers = new char[numbers.lenght];
+
+char min = charNumbers[0];
+int index = 0;
+for(int j = 0 ;j=charNumbers.length;j++){
+   for(int i = 0 ;i=charNumbers.length;i++){
+      if(min<charNumbers[i]){
+         index = i;
+         min = charNumbers[i];
+         }
+      }
+   resultNumbers[j]=min;
+   charNumbers[index] = (char)255;
+}
+* */
+
+
     }
 
     public static boolean isPrime(int x) {
